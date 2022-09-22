8aa2970c1683d6a26d4d4a8028b52a0a6f3b5011
Daniel ikechukwu <danielikechukwu495@gmail.com>
2022-09-22 20:45:30 +0100

    Add project files.

---
+++ Encapsulation/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+    namespace Encapsulation
+    {
+
+    class Area
+
+    {
+
+        public double length;
+        public double width;
+
+        public double GetArea()
+        {
+            return length * width;
+        }
+
+        public void Display()
+        {
+            Console.WriteLine("Length {0} : ", length);
+            Console.WriteLine("Width {0} : ", width);
+            Console.WriteLine("Total Area {0} : ", GetArea());
+
+        }
+    }
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Area area = new Area();
+            area.length = 3;
+            area.width = 4;
+            area.Display();
+            Console.ReadLine();
+
+        }
+    }
+}
